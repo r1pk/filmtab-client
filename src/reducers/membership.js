23a4015ede4f702f17bbcead95ca05cf2dc4d9c3
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-10-02 17:28:44 +0200

    Create reducer for membership functionality

---
+++ src/reducers/membership.js
@@ -0,0 +1,29 @@
+import { UPDATE_ROOM_STATE } from '../actions/membership';
+
+const initialReducerState = {
+  users: [],
+  video: {
+    url: 'https://www.youtube.com/watch?v=LXb3EKWsInQ',
+    playing: false,
+    playedSeconds: 0,
+    timestamp: new Date().getTime(),
+  },
+};
+
+export const membershipReducer = (state = initialReducerState, action) => {
+  switch (action.type) {
+    case UPDATE_ROOM_STATE: {
+      const { users, video } = action.payload.state;
+      return {
+        ...state,
+        users: [...users.values()].map((user) => ({
+          name: user.name,
+        })),
+        video: JSON.parse(JSON.stringify(video)),
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
