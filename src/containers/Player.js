29209a770bec68b149fde94791f309101a0cc349
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-10-14 23:37:55 +0200

    Disable eslint for useEffect dependency array

--- src/containers/Player.js
+++ src/containers/Player.js
@@ -19,6 +19,7 @@ const PlayerContainer = (props) => {
     if (isPlayerReady && playerRef.current) {
       playerRef.current.seekTo(playedSeconds);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [playedSeconds]);
 
   const handlePlayerReady = () => {
