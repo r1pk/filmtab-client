60a05584c8cef2fe9707646a6ce0cfd570fdaabc
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-10-14 16:42:41 +0200

    Create basic layout for Room page

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,7 +1,21 @@
-import { Grid } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
+
+import Player from '../containers/Player';
+import VideoAddressBar from '../containers/VideoAddressBar';
+import UserList from '../containers/UserList';
 
 const Room = () => {
-  return <Grid container justifyContent="center"></Grid>;
+  return (
+    <Grid container justifyContent="center" mt={1}>
+      <Grid item xs={11} md={10} lg={8} xl={6}>
+        <Stack spacing={1}>
+          <VideoAddressBar />
+          <Player />
+          <UserList />
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Room;
