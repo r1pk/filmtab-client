723649dc6004bec366157c5a1086e301178c48e7
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-10-21 18:50:30 +0200

    Create Notification component
    Create Notification component and container to display app notifications

---
+++ src/components/Notification/index.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+import { Snackbar, Alert, AlertTitle } from '@mui/material';
+
+const capitalizeFirstLetter = (string = '') => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const Notification = ({ type, message, onClose, ...rest }) => {
+  const handleCloseNotification = (e, reason = 'button') => {
+    if (onClose) {
+      onClose(e, reason);
+    }
+  };
+
+  return (
+    <Snackbar onClose={handleCloseNotification} {...rest}>
+      <Alert onClose={handleCloseNotification} severity={type} sx={{ width: '100%' }}>
+        {type && <AlertTitle>{capitalizeFirstLetter(type)}</AlertTitle>}
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+Notification.propTypes = {
+  type: PropTypes.oneOf(['error', 'warning', 'info', 'success']),
+  message: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+
+Notification.defaultProps = {
+  type: 'info',
+};
+
+export default Notification;
