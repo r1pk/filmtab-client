04db424df12b7a7094ee3b639f1421d2cd2d9c59
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-10-29 03:31:35 +0200

    Make SeekBar more user-friendly
    Create separate function to call onVideoSeek after the changes are committed

--- src/components/Player/Controls/SeekBar.js
+++ src/components/Player/Controls/SeekBar.js
@@ -1,15 +1,47 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Slider } from '@mui/material';
 
+const convertSecondsToHMS = (seconds) => {
+  const isoString = new Date(seconds * 1000).toISOString();
+  return seconds < 3600 ? isoString.substr(14, 5) : isoString.substr(11, 8);
+};
+
 const SeekBar = ({ progress, duration, onVideoSeek, ...rest }) => {
+  const [isChanging, setIsChanging] = useState(false);
+  const [localProgress, setLocalProgress] = useState(progress);
+
   const handleSliderChange = (e, value) => {
+    if (!isChanging) {
+      setIsChanging(true);
+    }
+    setLocalProgress(value);
+  };
+
+  const handleSliderChangeCommitted = (e, value) => {
     if (onVideoSeek) {
+      setIsChanging(false);
       onVideoSeek(value);
     }
   };
 
-  return <Slider size="small" value={progress} max={duration} onChange={handleSliderChange} {...rest} />;
+  const handleLabelValue = (value) => {
+    return convertSecondsToHMS(value);
+  };
+
+  return (
+    <Slider
+      size="small"
+      valueLabelDisplay="auto"
+      valueLabelFormat={handleLabelValue}
+      value={isChanging ? localProgress : progress}
+      max={duration}
+      onChange={handleSliderChange}
+      onChangeCommitted={handleSliderChangeCommitted}
+      {...rest}
+    />
+  );
 };
 
 SeekBar.propTypes = {
