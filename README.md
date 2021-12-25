7cb1f5317e52b0cc1636ea39f34f2fdd240256c8
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2021-12-25 23:36:27 +0100

    Change home page layout
    Change home page layout by adding tabs from the MUI library.
    The default displayed tab is selected based on whether URL contains
    the id of the room that user want to join.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Filmtab-client
 
-Filmtab-client is a responsive application written in React which uses tools such as [Redux](https://redux.js.org/), [Styled Components](https://styled-components.com/), [Material UI](https://mui.com/getting-started/usage/) and [Colyseus.js](https://www.colyseus.io/). Filmtab-client allows you to sychronically watch videos with your friends after you create or join a room.
+Filmtab-client is a responsive application written in React which uses tools such as [Redux](https://redux.js.org/), [Styled Components](https://styled-components.com/), [Material UI](https://mui.com/getting-started/usage/) and [Colyseus.js](https://www.colyseus.io/). Filmtab-client allows you to synchronically watch videos with your friends after you create or join a room.
 
 ## Screenshots
 
 Home page
-![Home page](https://i.imgur.com/SxmxjcD.png)
+![Home page](https://i.imgur.com/XYaH9AX.png)
 
 Room page with playing video
 ![Video player page](https://i.imgur.com/QmrGZwu.png)
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,28 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Tabs, Tab, Box } from '@mui/material';
 
 import { CreateRoomCardContainer, JoinRoomCardContainer } from '../features/room';
 
 import useDocumentTitle from '../hooks/useDocumentTitle';
 
 const Home = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const initialRoomId = location.state?.roomId;
+  const initialTab = initialRoomId ? 1 : 0;
+
+  const [activeTab, setActiveTab] = useState(initialTab);
+
   const isRoomMember = useSelector((store) => store.room.isRoomMember);
   const activeRoomId = useSelector((store) => store.room.activeRoomId);
 
-  const location = useLocation();
-  const navigate = useNavigate();
+  const handleChangeTab = (e, tabIndex) => {
+    setActiveTab(tabIndex);
+  };
 
   useEffect(() => {
     if (isRoomMember) {
@@ -31,10 +40,14 @@ const Home = () => {
         </Typography>
       </Grid>
       <Grid item>
-        <JoinRoomCardContainer defaultRoomId={location.state?.roomId} />
-      </Grid>
-      <Grid item>
-        <CreateRoomCardContainer />
+        <Tabs value={activeTab} onChange={handleChangeTab} centered>
+          <Tab label="Create room" />
+          <Tab label="Join room" />
+        </Tabs>
+        <Box p={1}>
+          {activeTab === 0 && <CreateRoomCardContainer />}
+          {activeTab === 1 && <JoinRoomCardContainer defaultRoomId={initialRoomId} />}
+        </Box>
       </Grid>
     </Grid>
   );
