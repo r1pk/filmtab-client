aa74dc960738984a49bb04d52f172636c63d6693
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-04-09 00:02:14 +0200

    Add welcome message after joining a room

--- src/features/chat/redux/reducer.js
+++ src/features/chat/redux/reducer.js
@@ -1,7 +1,16 @@
 import { RECEIVE_MESSAGE, CLEAR_CHAT } from './actions';
 
+const welcomeMessage = {
+  id: 'welcome_message',
+  author: {
+    name: 'FilmTab Bot',
+    color: 'rgb(255,215,0)',
+  },
+  content: 'Welcome to FilmTab!',
+  timestamp: new Date().getTime(),
+};
 const initialReducerState = {
-  messages: [],
+  messages: [welcomeMessage],
 };
 
 export const reducer = (state = initialReducerState, action) => {
@@ -12,7 +21,7 @@ export const reducer = (state = initialReducerState, action) => {
       return { ...state, messages: [...state.messages, message] };
     }
     case CLEAR_CHAT: {
-      return { ...state, messages: initialReducerState.messages };
+      return { ...state, messages: initialReducerState.messages.slice(1) };
     }
     default: {
       return state;
