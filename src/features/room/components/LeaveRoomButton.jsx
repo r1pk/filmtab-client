57b317f3833946c041476ef072c3b673d777247e
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-06-16 23:42:50 +0200

    Improve LeaveRoomButton component
    - Add `ExitToApp` icon to LeaveRoomButton.
    - Component now ask for user confirmation before leaving the room.

--- src/features/room/components/LeaveRoomButton.jsx
+++ src/features/room/components/LeaveRoomButton.jsx
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 
+import { ExitToApp } from '@mui/icons-material';
+
 import Button from '../../../components/Button';
 
 const LeaveRoomButton = ({ onLeaveRoom, ...rest }) => {
   const handleLeaveRoom = () => {
-    onLeaveRoom();
+    const userConfirmed = window.confirm('Are you sure you want to leave the room?');
+
+    if (userConfirmed) {
+      onLeaveRoom();
+    }
   };
 
   return (
-    <Button variant="text" color="error" onClick={handleLeaveRoom} {...rest}>
+    <Button variant="text" color="error" startIcon={<ExitToApp />} onClick={handleLeaveRoom} {...rest}>
       Leave room
     </Button>
   );
