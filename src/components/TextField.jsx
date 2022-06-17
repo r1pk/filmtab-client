dc034dc6503c65334cd5b07ffb7f0745a367ae3c
Patryk Krawczyk <patrykekrawczyk@gmail.com>
2022-06-17 23:02:18 +0200

    Fix the use of forwardRef in TextField component
    - Fix the use of forwardRef in TextField component by sticking
      to the forwardRef docs.
    - Add displayName to the TextField component.

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -2,8 +2,10 @@ import { forwardRef } from 'react';
 
 import { TextField as MUITextField } from '@mui/material';
 
-const TextField = (props, ref) => {
+const TextField = forwardRef((props, ref) => {
   return <MUITextField variant="standard" size="small" {...props} ref={ref} />;
-};
+});
 
-export default forwardRef(TextField);
+TextField.displayName = 'TextField';
+
+export default TextField;
